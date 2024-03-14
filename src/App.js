5cc7d32df8feacc6f9c17f1a19d51935382d6faf
Anany Pathak <propanany@gmail.com>
2024-03-14 22:12:00 +0530

    Initial Logic

--- src/App.js
+++ src/App.js
@@ -1,30 +1,46 @@
-import './App.css';
-import Table from './components/Table.js'
+import { useState } from "react";
+import "./App.css";
+import Table from "./components/Table";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([]);
+  
+  const handleSubmit = () => {
+    console.log(input);
+    setInput("");
+  };
+
+  const handleAddTask = () => {
+    if (input.trim() !== '')
+      setTasks([...tasks, input]);
+    setInput('');
+  };
+  
+
   return (
     <div className="main-container">
       <div>
-        <div>
+        <div className="heading">
           <h1>ToDo List Web Application</h1>
         </div>
-        <div className='input-text'>
-          <input placeholder='Enter the task' className='input'/>
+        <div className="input-text">
+          <input
+            placeholder="Enter the task"
+            className="input"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
         </div>
-        <button type='submit' className='input-btn'>Add</button>
-        
+        <button className="input-btn" onClick={handleAddTask}>
+          Add
+        </button>
       </div>
       <div>
-        <Table />
+        <Table tasks={tasks}/>
       </div>
     </div>
   );
 }
 
 export default App;
-
-
-
-      
-      
-      
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,49 +1,34 @@
-import React from 'react'
-import './Table.css'
-import Delete from "../../src/delete.png"
-import Edit from "../../src/edit.png"
+import React from "react";
+import "./Table.css";
+import Delete from "../../src/delete.png";
+import Edit from "../../src/edit.png";
 
-function Table() {
+function Table({tasks}) {
   return (
-    <div className='task-table'>
-      <table border='1' >
-        <thead>
-            <th>S. No.</th>
-            <th>Pending tasks</th>
-            <th>Action</th>
+    <div className="task-table">
+      <table>
+        <thead className="thead">
+          <th>S. No.</th>
+          <th>Pending tasks</th>
+          <th>Action</th>
         </thead>
-        <tbody>
-            <tr>
-                <td>1</td>
-                <td>Make todo List</td>
-                
-                <td>
-                    <img src={Delete} className='btn-img'></img>
-                    <img src={Edit} className='btn-img'></img>
-                </td>
-                
+        <tbody className="tbody">
+          {tasks.map((task, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{task}</td>
+              <td>
+                <img src={Delete} className="btn-img"></img>
+                <img src={Edit} className="btn-img"></img>
+              </td>
             </tr>
-            <tr>
-                <td>1</td>
-                <td>Make todo List</td>
-                <td>
-                    <img src={Delete} className='btn-img'></img>
-                    <img src={Edit} className='btn-img'></img>
-                </td>
-            </tr>
-            <tr>
-                <td>1</td>
-                
-                <td>Make todo List</td>
-                <td>
-                    <img src={Delete} className='btn-img'></img>
-                    <img src={Edit} className='btn-img'></img>
-                </td>
-            </tr>
+          ))}
         </tbody>
       </table>
     </div>
-  )
+  );
 }
 
-export default Table
+export default Table;
+
+
