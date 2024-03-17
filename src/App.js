44e933484f634aecff0c2e7e69aecd1f5f2cb443
Anany Pathak <propanany@gmail.com>
2024-03-17 14:48:10 +0530

    API Implementation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import "./App.css";
 import Table from "./components/Table";
 
 function App() {
   const [input, setInput] = useState("");
   const [tasks, setTasks] = useState([]);
-  const [editIndex, setEditIndex] = useState(null); 
+  const [editIndex, setEditIndex] = useState(null);
+  const [apiResponse, setApiResponse] = useState(null);
+
+  useEffect(() => {
+    fetch('https://susan-hacker-solomon-alike.trycloudflare.com/api/1.0/feature/config')
+    .then(response => response.json())
+    .then(data => {
+      setApiResponse(data);
+    });
+  });
+
+  const isEditable = apiResponse ? apiResponse.isEditable : false;
 
   const handleAddTask = () => {
-    if (input.trim() !== '') {
-      if (editIndex !== null) { 
-        const updatedTasks = [...tasks];
-        updatedTasks[editIndex] = input;
-        setTasks(updatedTasks);
-        setEditIndex(null); 
-      } else {
-        setTasks([...tasks, input]); 
+    if (input !== "") {
+      if (!tasks.includes(input)) {
+        if (editIndex !== null) {
+          const updatedTasks = [...tasks];
+          updatedTasks[editIndex] = input;
+          setTasks(updatedTasks);
+          setEditIndex(null);
+        } else {
+          setTasks([...tasks, input]);
+        }
+      }
+      else {
+        alert("The task already exists !!! ")
       }
     }
-    setInput('');
+    setInput("");
   };
 
   const handleEditButton = (index, task) => {
-    setInput(task); 
-    setEditIndex(index); 
+    setInput(task);
+    setEditIndex(index);
   };
 
   const handleDeleteTask = (index) => {
@@ -31,6 +47,14 @@ function App() {
     updatedTasks.splice(index, 1);
     setTasks(updatedTasks);
   };
+
+  const handleDeleteOnFalse = (index) => {
+    alert("You are not authorised to delete !")
+  }
+
+  const handleEditOnFalse = (index, task) => {
+    alert("You are not authorised to edit !")
+  }
 
   return (
     <div className="main-container">
@@ -47,11 +71,19 @@ function App() {
           />
         </div>
         <button className="input-btn" onClick={handleAddTask}>
-          {editIndex !== null ? "Edit Task" : "Add"} 
+          {editIndex !== null ? "Edit Task" : "Add"}
         </button>
       </div>
       <div>
-        <Table tasks={tasks} onDelete={handleDeleteTask} onEdit={handleEditButton}/>
+        {(isEditable) ? 
+        (<Table
+        tasks={tasks}
+        onDelete={handleDeleteTask}
+        onEdit={handleEditButton}
+      />) : <Table
+      tasks={tasks}
+      onDelete={handleDeleteOnFalse}
+      onEdit={handleEditOnFalse} />}
       </div>
     </div>
   );
